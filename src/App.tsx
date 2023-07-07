5ae0d345c445382f2adc3796751c460828bd3755
Rohit1415 <bhaturohit549@gmail.com>
2023-07-07 15:49:29 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  interface User  {
+    name: string,
+    age: number,
+    email: string
+  }
+  const u1 :User = {
+    name:'rohit',
+    age:26,
+    email:"<EMAIL>"
+  }
+  console.log(u1)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      hello world
     </div>
   );
 }
